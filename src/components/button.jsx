b92f8bcf6b5487852cd292fbd510cddd588c93ef
Daniela Becerra <140854855+daniela1234becerra@users.noreply.github.com>
2024-02-27 19:26:44 -0500

    Add files via upload

---
+++ src/components/button.jsx
@@ -0,0 +1,17 @@
+import '../sass/button.scss'
+
+
+const Button = ({icon, handleClick}) => {
+  return (
+    <div className='button__box'>
+        <button 
+          className="button" 
+          onClick={handleClick}
+          >{icon}
+        </button>
+        <div className='button__shadow'></div>
+    </div>
+  )
+}
+
+export {Button}
